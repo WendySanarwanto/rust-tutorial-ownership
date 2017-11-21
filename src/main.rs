f6315aabe8fa701a4a08c2d27442d2dd2602f20a
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-11-22 04:28:40 +0800

    Added 'move ownership to function' demo.

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,25 @@ fn clone_demo() {
     pause();
 }
 
+fn display_text(text: String) {
+    println!("----> 'display_text' displays the 'text' argument as '{}'", text);
+}
+
+// Perform demo #3: Assign a heap variable into a function's argument will also move the variable's ownership.
+fn function_demo() {
+    println!("3. Assign heap variable as a function's argument");
+    println!("-------------------------------------------------\n");
+
+    let mut s1 = String::from("hello world!");
+    println!("Given, 's1' is '{}' and it is allocated in the Heap.", s1);
+    println!("And we have 'display_text' method which takes a String type argument");
+    println!("When we assign 's1' as the argument of 'display_text' method");
+    println!("Then the ownership of 's1' is also moved to 'display_text' method.");
+    display_text(s1);
+    // Try enable this following line and you'll get compile error
+    // s1.push_str("\n");
+}
+
 // Main entry
 fn main() {
     println!("\nRust Ownership's Demo.");
@@ -46,4 +65,6 @@ fn main() {
     move_demo();
 
     clone_demo();
+
+    function_demo();
 }
